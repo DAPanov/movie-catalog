5fba76008f0ae739fe31135d0f4f10f4092d285c
Panov Dmitriy <dma.panov@gmail.com>
2025-04-24 14:31:12 +0300

    Add root view

--- movie-catalog/main.py
+++ movie-catalog/main.py
@@ -1,5 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI(
     title="Movie catalog",
 )
+
+@app.get("/")
+async def root(request: Request,):
+    docs_url = request.url.replace(path="/docs")
+    return {"message": "Hello World",
+            "docs": str(docs_url),}
