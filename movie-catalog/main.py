b34f9a6cc074e59e9f3d6d90e482f661bf933af9
Panov Dmitriy <dma.panov@gmail.com>
2025-04-25 12:07:43 +0300

    Add get_movie-list view and get_movie_by_id view

--- movie-catalog/main.py
+++ movie-catalog/main.py
@@ -1,11 +1,53 @@
 from fastapi import FastAPI, Request
 
+from schemas.movie import Movie
+
 app = FastAPI(
     title="Movie catalog",
 )
 
+MOVIE_LIST = [
+    Movie(
+        id=1,
+        title="The Shawshank Redemption",
+        description="A banker convicted of uxoricide forms a friendship over a quarter century with a hardened convict,\n"
+        " while maintaining his innocence and trying to remain hopeful through simple compassion.",
+        year=1994,
+    ),
+    Movie(
+        id=2,
+        title="The Godfather",
+        description="The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+        year=1972,
+    ),
+    Movie(
+        id=3,
+        title="The Dark Knight",
+        description="When a menace known as the Joker wreaks havoc and chaos on the people of Gotham,\n"
+        " Batman, James Gordon and Harvey Dent must work together to put an end to the madness.",
+        year=2008,
+    ),
+]
+
+
 @app.get("/")
-async def root(request: Request,):
+async def root(
+    request: Request,
+):
     docs_url = request.url.replace(path="/docs")
-    return {"message": "Hello World",
-            "docs": str(docs_url),}
+    return {
+        "message": "Hello World",
+        "docs": str(docs_url),
+    }
+
+
+@app.get("/movie-list/", response_model=list[Movie])
+@app.get("/movie-list", response_model=list[Movie])
+async def get_movies_list() -> list[Movie]:
+    return MOVIE_LIST
+
+
+@app.get("/movie/{movie_id}", response_model=Movie)
+async def get_movie_by_id(movie_id: int) -> Movie:
+    movie = next((movie for movie in MOVIE_LIST if movie.id == movie_id), None)
+    return movie
