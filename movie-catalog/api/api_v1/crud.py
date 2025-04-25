9728272d149bc29737091d585aab35280152a2e5
Panov Dmitriy <dma.panov@gmail.com>
2025-04-25 13:13:04 +0300

    Move movie data to crud.py module

---
+++ movie-catalog/api/api_v1/crud.py
@@ -0,0 +1,24 @@
+from schemas.movie import Movie
+
+MOVIE_LIST = [
+    Movie(
+        id=1,
+        title="The Shawshank Redemption",
+        description="A banker convicted of uxoricide forms a friendship over a quarter century with a hardened convict,\n"
+        " while maintaining his innocence and trying to remain hopeful through simple compassion.",
+        year=1994,
+    ),
+    Movie(
+        id=2,
+        title="The Godfather",
+        description="The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+        year=1972,
+    ),
+    Movie(
+        id=3,
+        title="The Dark Knight",
+        description="When a menace known as the Joker wreaks havoc and chaos on the people of Gotham,\n"
+        " Batman, James Gordon and Harvey Dent must work together to put an end to the madness.",
+        year=2008,
+    ),
+]
