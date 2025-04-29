04ca02d6178fe3842493809deccbade2cca45938
Panov Dmitriy <dma.panov@gmail.com>
2025-04-29 15:52:08 +0300

    Add DescriptionString

--- movie-catalog/schemas/movie.py
+++ movie-catalog/schemas/movie.py
@@ -1,8 +1,17 @@
+from typing import Annotated
+
+from annotated_types import MaxLen
 from pydantic import BaseModel, Field
 
 
+DescriptionString = Annotated[
+    str,
+    MaxLen(255),
+]
+
+
 class MovieBase(BaseModel):
-    description: str = ""
+    description: DescriptionString = ""
     year: int = ""
 
 
@@ -20,8 +29,17 @@ class MovieUpdate(MovieBase):
     Модель для обновления фильма
     """
 
-    description: str
+    description: DescriptionString
     year: int
+
+
+class MoviePartialUpdate(MovieBase):
+    """
+    Модель для частичного обновления фильма
+    """
+
+    description: DescriptionString | None = None
+    year: int | None = None
 
 
 class MovieCreate(Movie):
