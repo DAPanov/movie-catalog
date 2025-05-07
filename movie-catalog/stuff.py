5b5e9a41d5c32d2c2bc1d304037706f95aa76033
Panov Dmitriy <dma.panov@gmail.com>
2025-05-07 11:43:16 +0300

    Connect and check redis

---
+++ movie-catalog/stuff.py
@@ -0,0 +1,21 @@
+from redis import Redis
+
+from core import config
+
+redis = Redis(
+    host=config.REDIS_HOST,
+    port=config.REDIS_PORT,
+    db=config.REDIS_DB,
+    decode_responses=True,
+)
+
+
+def main():
+    print(redis.ping())
+    redis.set("name", "foo")
+    print(redis.get("name"))
+    redis.delete("name")
+    print(redis.get("name"))
+
+if __name__ == "__main__":
+    main()
