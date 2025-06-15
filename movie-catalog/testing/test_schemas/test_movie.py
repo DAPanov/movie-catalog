56b9affbc7d32b909460de6e4d955ab4ac946adc
Panov Dmitriy <dma.panov@gmail.com>
2025-06-15 12:00:11 +0300

    add movie update schema test

--- movie-catalog/testing/test_schemas/test_movie.py
+++ movie-catalog/testing/test_schemas/test_movie.py
@@ -17,13 +17,24 @@ class MovieCreateTestCase(TestCase):
         self.assertEqual(movie_in.year, movie.year)
         self.assertEqual(movie_in.slug, movie.slug)
 
+
+class MovieUpdateTestCase(TestCase):
     def test_movie_can_be_updated_from_update_schema(self) -> None:
-        movie_update = MovieUpdate(
+        movie = Movie(
             title="Movie",
             description="Description",
             year=1999,
+            slug="movie",
         )
-        movie = Movie(**movie_update.model_dump(), slug="slug")
+
+        movie_update = MovieUpdate(
+            title="Movie1",
+            description="Description1",
+            year=1920,
+        )
+
+        for field_name, value in movie_update:
+            setattr(movie, field_name, value)
 
         self.assertEqual(movie_update.title, movie.title)
         self.assertEqual(movie_update.description, movie.description)
